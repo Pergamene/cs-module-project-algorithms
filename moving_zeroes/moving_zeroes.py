2b4cbaba600b77f1465fe3650e91055c51cc2272
Pergamene <smartz373@gmail.com>
2020-07-09 18:31:08 -0600

    fixed moving zeros to be in place

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,13 +4,16 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
   # Your code here
-  newList = []
-  for num in arr:
-    if num == 0:
-      newList.append(num)
+  index = 0
+  length = len(arr)
+  while index < length:
+    if arr[index] == 0:
+      arr.pop(index)
+      arr.append(0)
+      length -= 1
     else:
-      newList.insert(0, num)
-  return newList
+      index += 1
+  return arr
 
 if __name__ == '__main__':
   # Use the main function here to test out your implementation
