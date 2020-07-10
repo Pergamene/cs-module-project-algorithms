5ee08ca8251c222347c32e18dc910cd29a41281c
Pergamene <smartz373@gmail.com>
2020-07-10 11:42:41 -0600

    fixedd sliding window for large input

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,13 +1,28 @@
+from collections import deque
+
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-  # Your code here
-  length = len(nums) - k + 1
   maxes = []
-  for i in range(length):
-    maxes.append(max(nums[i:i+k]))
+  dq = deque()
+
+  for i in range(k):
+    while dq and nums[i] >= nums[dq[-1]]:
+      dq.pop()
+    dq.append(i)
+
+  for i in range(k, len(nums)):
+    maxes.append(nums[dq[0]])
+    while dq and dq[0] <= i-k:
+      dq.popleft()
+    while dq and nums[i] >= nums[dq[-1]]:
+      dq.pop()
+    dq.append(i)
+  
+  maxes.append(nums[dq[0]])
+
   return maxes
 
 if __name__ == '__main__':
