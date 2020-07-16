b5b823da1f96539747b075d784996cde2b954288
Pergamene <smartz373@gmail.com>
2020-07-16 17:27:06 -0600

    eating cookies passing except n=100 and n=500

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,10 +1,36 @@
+from math import factorial, floor
+
 '''
 Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-  # Your code here
-  
+  if n == 0:
+    return 1
+  counts = _generate_counts(n)
+  return _find_permunations(counts)
+
+def _generate_counts(n):
+  all_counts = set()
+  max_threes = floor(n / 3)
+  max_twos = floor(n / 2)
+  for threes in range(max_threes+1):
+    for twos_max in range(max_twos+1):
+      twos = twos_max
+      while (3 * threes + 2 * twos) > n:
+        twos -= 1
+      ones = n - 3 * threes - 2 * twos
+      total = threes + twos + ones
+      all_counts.add((total, threes, twos, ones))
+  return all_counts
+
+def _find_permunations(counts):
+  count_total = 0
+  for count in counts:
+    total, threes, twos, ones = count
+    permutations = factorial(total) / (factorial(threes) * factorial(twos) * factorial(ones))
+    count_total += int(permutations)
+  return count_total 
 
 if __name__ == "__main__":
   # Use the main function here to test out your implementation
